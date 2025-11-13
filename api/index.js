b4b72c03f5c7b3e20acab6cee1c759f7fb306820
Jessen <jessen123ptk@gmail.com>
2025-11-13 22:58:58 +0800

    fix vercel json

--- api/index.js
+++ api/index.js
@@ -1,35 +1,3 @@
-// // api/index.js
-// /* eslint-disable @typescript-eslint/no-require-imports */
-// const path = require('path');
-
-// module.exports = async (req, res) => {
-//   try {
-//     const appPath = path.join(__dirname, '../dist/main.js');
-//     const { default: app } = await import(appPath);
-//     return app(req, res);
-//   } catch (err) {
-//     console.error('Error loading NestJS app:', err);
-//     res.status(500).send('NestJS app not built or failed to load.');
-//   }
-// };
-
-// import path from 'path';
-// import { fileURLToPath } from 'url';
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-// export default async function handler(req, res) {
-//   try {
-//     const appPath = path.join(__dirname, '../dist/main.js');
-//     const { default: app } = await import(appPath);
-//     return app(req, res);
-//   } catch (err) {
-//     console.error('Error loading NestJS app:', err);
-//     res.status(500).send('NestJS app not built or failed to load.');
-//   }
-// }
-
 import fs from 'fs';
 import path from 'path';
 
