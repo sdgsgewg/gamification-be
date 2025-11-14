11eb779a089b9bc2fb2119d4b7f8a1abc11291e9
Jessen <jessen123ptk@gmail.com>
2025-11-14 12:54:26 +0800

    fix main.ts

--- src/main.ts
+++ src/main.ts
@@ -46,24 +46,69 @@
 
 // --- SETUP KHUSUS UNTUK DEPLOY DI VERCEL ---
 
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { ConfigService } from '@nestjs/config';
+// import { ValidationPipe } from '@nestjs/common';
+// import cookieParser from 'cookie-parser';
+// import express, { Request, Response } from 'express';
+// import { ExpressAdapter } from '@nestjs/platform-express';
+// import { json, urlencoded } from 'express';
+
+// async function createApp() {
+//   const expressApp = express();
+//   const app = await NestFactory.create(
+//     AppModule,
+//     new ExpressAdapter(expressApp),
+//   );
+//   const configService = app.get(ConfigService);
+
+//   // app.setGlobalPrefix('api');
+//   app.use(cookieParser());
+//   app.use(json({ limit: '10mb' }));
+//   app.use(urlencoded({ extended: true, limit: '10mb' }));
+
+//   app.useGlobalPipes(
+//     new ValidationPipe({
+//       transform: true,
+//       whitelist: true,
+//     }),
+//   );
+
+//   app.enableCors({
+//     origin: configService.get('FRONTEND_URL') || '*',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control'],
+//     credentials: true,
+//   });
+
+//   await app.init();
+//   return expressApp;
+// }
+
+// // Pastikan default export berupa function, bukan Promise
+// const handler = async (req: Request, res: Response) => {
+//   const app = await createApp();
+//   return app(req, res);
+// };
+
+// module.exports = handler;
+
+// --- SETUP KHUSUS UNTUK DEPLOY DI RAILWAY ---
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
-import express, { Request, Response } from 'express';
-import { ExpressAdapter } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
 import { json, urlencoded } from 'express';
 
-async function createApp() {
-  const expressApp = express();
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(expressApp),
-  );
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
-  // app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api');
+
   app.use(cookieParser());
   app.use(json({ limit: '10mb' }));
   app.use(urlencoded({ extended: true, limit: '10mb' }));
@@ -76,20 +121,16 @@ async function createApp() {
   );
 
   app.enableCors({
-    origin: configService.get('FRONTEND_URL') || '*',
+    origin:
+      configService.get('FRONTEND_URL') ||
+      'https://gamification-fe-phi.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control'],
     credentials: true,
   });
 
-  await app.init();
-  return expressApp;
+  await app.listen(process.env.PORT || 3001);
+  console.log(`App running on ${await app.getUrl()}`);
 }
 
-// Pastikan default export berupa function, bukan Promise
-const handler = async (req: Request, res: Response) => {
-  const app = await createApp();
-  return app(req, res);
-};
-
-module.exports = handler;
+bootstrap();
